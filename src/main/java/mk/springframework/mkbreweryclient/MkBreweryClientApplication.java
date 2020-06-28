3bf311d9a5502f8852eb47a839cfefc8c55dec8c
MadanKumar <madan739@gmail.com>
2020-06-28 18:44:06 +0530

    Initial Project Add

---
+++ src/main/java/mk/springframework/mkbreweryclient/MkBreweryClientApplication.java
@@ -0,0 +1,13 @@
+package mk.springframework.mkbreweryclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MkBreweryClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MkBreweryClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mk/springframework/mkbreweryclient/MkBreweryClientApplicationTests.java
@@ -0,0 +1,13 @@
+package mk.springframework.mkbreweryclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class MkBreweryClientApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
